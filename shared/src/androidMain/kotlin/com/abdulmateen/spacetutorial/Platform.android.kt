403675281e4bc3d64c183e107ced44184fcfd2da
Abdulmateen <abdulmateen6.ch@gmail.com>
2024-05-23 08:57:22 +0500

    Initial commit

---
+++ shared/src/androidMain/kotlin/com/abdulmateen/spacetutorial/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.abdulmateen.spacetutorial
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
